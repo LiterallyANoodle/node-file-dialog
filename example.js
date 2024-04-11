e1510ed62d6e9a3d755e018968905d6dff6b0e6d
LiterallyANoodle <themaskedkid321@gmail.com>
2024-04-11 09:47:25 -0500

    js run with filetype filter

--- example.js
+++ example.js
@@ -1,5 +1,9 @@
 const dialog = require('node-file-dialog')
 const config = {
-  type: 'directory'
+  type: 'directory',
+  filetypes: { 
+    'Executable': '*.exe',
+    'All files': '*.*',
+  }
 };
 dialog(config).then(dir => console.log(dir)).catch(err => console.log(err))
